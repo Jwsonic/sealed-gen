c0deeca08ff4d5671150df1b7536525f4a4ab5f7
John Palgut <Jwsonic@gmail.com>
2014-09-12 18:07:31 -0500

    working release

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,34 +14,40 @@ function httpGet($http, url) {
  *
  * Main module of the application.
  */
-angular
-  .module('sealedGenApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        resolve: {
-          factions: ['$http',
-            function($http) {
-              return httpGet($http, 'factions.json');
-            }
-          ],
-          cards: ['$http',
-            function($http) {
-              return httpGet($http, 'cards.json');
-            }
-          ]
-        }
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var app = angular.module('sealedGenApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch'
+]).config(function($routeProvider) {
+  console.log('hello');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      resolve: {
+        factions: ['$http',
+          function($http) {
+            return httpGet($http, 'factions.json');
+          }
+        ],
+        cards: ['$http',
+          function($http) {
+            return httpGet($http, 'cards.json');
+          }
+        ]
+      }
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}).config([
+  '$compileProvider',
+  function($compileProvider) {
+    console.log('hello');
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|blob|chrome-extension):/);
+    // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+  }
+]);
